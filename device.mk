19fee718246aa73c5ea69047515162e801a91a5b
isus203 <128573607+isus203@users.noreply.github.com>
2024-07-30 15:25:31 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -142,4 +142,4 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
 # Inherit the proprietary files
-$(call inherit-product, vendor/tecno/KH7n/KH7n-vendor.mk)
+$(call inherit-product, vendor/TECNO/KH7n/KH7n-vendor.mk)
