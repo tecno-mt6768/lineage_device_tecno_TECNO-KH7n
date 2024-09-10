fd29a9b2e9c8be7039561b7c438d669e7bae15ba
isus203 <128573607+isus203@users.noreply.github.com>
2024-09-10 03:48:03 +0300

    Update Android.mk

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -14,3 +14,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.recovery.mt6768.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := factory_init.connectivity.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init/hwfactory_init.connectivity.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
