7136e8e2db317015a772ed4778a7660d56503ccd
isus203 <128573607+isus203@users.noreply.github.com>
2024-09-10 04:34:38 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,5 +161,7 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/config/prop/vendor.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/config/prop/product.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/config/prop/system_ext.prop
 
+BOARD_VNDK_VERSION := current
+
 # Inherit the proprietary files
 include vendor/TECNO/KH7n/BoardConfigVendor.mk
